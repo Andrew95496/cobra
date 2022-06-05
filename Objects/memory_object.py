883938bac081146e16c7c6d955914114f5f7c6e0
Andrew Leacock <andrewleacock96@gmail.com>
2022-06-05 16:54:20 -0400

    add DUMP and DUMP_ALL method to Objects/memory_object.py

--- Objects/memory_object.py
+++ Objects/memory_object.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 @dataclass
 class Memory:
 
-    def __init__(self, address, raw_data, data, initialization):
+    def __init__(self, address=None, raw_data=None, data=None, initialization=None):
         self.address = address
         self.raw_data = raw_data
         self.data = data
@@ -13,6 +13,16 @@ class Memory:
     def __repr__(self) -> str:
         rep = f'''Address: {self.address}\nRaw Contents: {self.raw_data}\nContents:\n{self.data}\nTime: {self.initialization}\n'''
         return rep
+
+    def DUMP_ALL(self):
+        with open('.memory', 'w') as MEMORY, open('State/.address', 'w' ) as ADDRESS:
+            ADDRESS.write('')
+            MEMORY.write('')
+        print('MEMORY DUMPED')
+        
+
+    def DUMP(self, address):
+        return f'MEMORY DUMPED AT {address}'
 
     # get memory
     def get():
