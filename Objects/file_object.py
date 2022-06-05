b3f77f89c5484c2277a320f34e855a3081f42728
Andrew Leacock <andrewleacock96@gmail.com>
2022-06-05 16:33:09 -0400

    add append method to file_object.py

--- Objects/file_object.py
+++ Objects/file_object.py
@@ -1,24 +1,24 @@
 from dataclasses import dataclass
-import pandas
-
+import itertools as its
 @dataclass
 class File:
     
     def __init__(self, filename):
         self.filename = filename
 
-    def write(self, data):
+    def append(self, data):
         with open(f'{self.filename}', 'a') as file: 
+            file.write(data)
+
+    def write(self, data):
+        with open(f'{self.filename}', 'w') as file: 
             file.write(data)
         
     def write_excel(self, data):
-        data.to_excel(f'{self.filename}.xlsx')
-        print(f'{self.filename}')
+        data.to_excel(f'{self.filename}')
+        # print(f'{self.filename}')
 
     def read(self):
-        LINES = []
-        with open(f'{self.filename}', 'r') as file: 
-            for line in file:
-                line = line.rstrip('\n')
-                LINES.append(line)
+        with open(f'{self.filename}', 'r') as file:
+            LINES = [line.rstrip('\n') for line in file] 
         return LINES
